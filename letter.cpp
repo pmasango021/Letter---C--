3f763439dc8a87e4f651af9de0bf1793eb811786
pmasango021 <103104826+pmasango021@users.noreply.github.com>
2022-11-13 00:34:39 +0200

    Refactored letter.cpp

---
+++ letter.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Dear Self\n"
+                 "\n"
+                 "\n"
+                 "An open letter to myself.\n"
+                 "Your scars will heal and on days when you poke it, it will cause no pain. You will find solace in your past, the memories that sting and the bumpy ride because it is part of the process of being your better self.\n"
+                 "\n"
+                 "You will find strength to look into the eyes of those that hurt you, take away their power to because now, you call the shots. You will find kindness and warmth in the hands of strangers and friendship in the most unexpected places.\n"
+                 "\n"
+                 "You find your own person, you will find love and companionship and oh... it will change you. Feel, care, love and experience undeniable miracles and purpose in your life. Yield to things greater than your ambition; to friendships, to companionship, to shots. Free yourself from the weights that you have tied to your ankles and the unending list of responsibilities that have weighed you down. You deserve to fill your heart with wonder. Experience the beauty that has been created for you to enjoy.\n"
+                 "\n"
+                 "I hope you experience the happiness that exists on your own terms. I hope you have the courage to accept that you are deserving of everything you desire, that you’re capable of curating the kind of life that sparks something within you. I hope you find genuine love. The kind that makes you a softer person because nothing is more beautiful than loving someone who builds you a home in their heart. Let your guards down and let it in.\n"
+                 "\n"
+                 "I hope you find the kind of moments that take your breath away. I hope you surround yourself with friends that encourage your passions and spontaneity. I hope you connect with the small things.I hope you meet a confidant. I hope you find all the things you yearn for.\n"
+                 "\n"
+                 "Above all, I hope you find yourself out there. I hope you learn how to be kind to yourself, how to embrace the journey you are on. I hope you fall in love with the process of life, the messiness and confusion of it all.\n"
+                 "\n"
+                 "I hope your life inspires you.\n"
+                 "\n"
+                 "\n"
+                 "Phillip Sebole Masango";
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//
+//int main()
+//{
+//
+//    std::cout << "Hello\n";
+//    std::cout << "Goodbye\n";
+//    std::cout << "       1\n"
+//                 "     2 3\n"
+//                 "   4 5 6\n"
+//                 "7 8 9 10";
+//
+//
+//
+//}
