1032ee25877e9611041a1be42f1bbbd44405c9f4
Phillip Sebole Masango <103104826+pmasango021@users.noreply.github.com>
2022-11-13 00:36:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,5 @@
 //
 //int main()
 //{
-//
-//    std::cout << "Hello\n";
-//    std::cout << "Goodbye\n";
-//    std::cout << "       1\n"
-//                 "     2 3\n"
-//                 "   4 5 6\n"
-//                 "7 8 9 10";
-//
-//
-//
+
 //}
